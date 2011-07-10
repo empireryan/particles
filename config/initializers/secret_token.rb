d9c1bf56ceeb2c2f21eeb3c6e268e4eadaddbbde
Ryan Rodriguez <Empireryan@gmail.com>
2011-07-09 18:22:53 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Particles::Application.config.secret_token = '1a053a375e3a1c58c5fa5d497b4f8c96937f39d035f293b6f5ca5f41c3123ba3ff58bd6bba3f2229cf47f2f4e768f229d3657525abe7b96c8664fcbeb8f1b3a3'
